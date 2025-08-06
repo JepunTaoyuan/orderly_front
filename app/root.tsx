0a87d11a6edc6e0d732aee1c4a9304e653b05484
Jame Zhan <jame@orderly.network>
2025-08-06 23:06:48 +0800

    move image assets to images dir

--- app/root.tsx
+++ app/root.tsx
@@ -24,7 +24,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="icon" type="image/png" href="/images/favicon.png" />
         <Meta />
         <Links />
       </head>
