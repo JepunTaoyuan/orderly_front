8fcccd64a50cae069fe055e1066f8ee44ac8e3e2
KCL1104 <charlie011111@gmail.com>
2025-12-01 14:27:05 +0800

    Apply design for vaults page

--- app/packages/vaults/pages/index.tsx
+++ app/packages/vaults/pages/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import { cn, useScreen } from "@orderly.network/ui";
+import { useScreenSize } from "@/hooks/custom/useScreenSize";
 import { VaultsProvider } from "../components/provider/vaults-provider";
 import type { VaultsPageConfig } from "../types/vault";
 
@@ -28,22 +29,24 @@ export type VaultsPageProps = {
 
 export const VaultsPage: FC<VaultsPageProps> = (props) => {
   const { isMobile } = useScreen();
+  const { size } = useScreenSize();
   return (
     <VaultsProvider {...props}>
       <div
         className={cn(
           "oui-relative oui-min-h-screen ",
           "oui-bg-base-10",
-          isMobile ? "oui-px-3 oui-py-6" : "oui-px-6 oui-py-12",
+          isMobile ? "oui-py-6" : "oui-py-6",
           props?.className,
         )}
       >
         <div
           id="vaults-content"
-          className={cn(
-            "oui-mx-auto oui-flex oui-max-w-[1200px] oui-flex-col",
-            isMobile ? "oui-gap-6" : "oui-gap-12",
-          )}
+          className={cn("oui-mx-auto oui-flex-col")}
+          style={{
+            paddingLeft: size === "1920+" ? "240px" : "12px",
+            paddingRight: size === "1920+" ? "240px" : "12px",
+          }}
         >
           <React.Suspense fallback={null}>
             <LazyVaultsHeaderWidget />
