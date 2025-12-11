def8606f4aeef84e7f2df671e498906c382350a1
KCL1104 <charlie011111@gmail.com>
2025-12-11 10:16:41 +0800

    Update order entry

---
+++ app/packages/ui-order-entry/components/orderInput/limit/priceLabelButtons.tsx
@@ -0,0 +1,71 @@
+import { useTranslation } from "@orderly.network/i18n";
+import { Flex, cn, modal, Text } from "@orderly.network/ui";
+import { OrderEntryScriptReturn } from "../../../orderEntry.script";
+import { BBOStatus } from "../../../utils";
+
+type PriceLabelButtonsProps = {
+  bbo: Pick<
+    OrderEntryScriptReturn,
+    "bboStatus" | "bboType" | "onBBOChange" | "toggleBBO"
+  >;
+  fillMiddleValue: OrderEntryScriptReturn["fillMiddleValue"];
+};
+
+export const PriceLabelButtons = (props: PriceLabelButtonsProps) => {
+  const { bbo, fillMiddleValue } = props;
+  const { t } = useTranslation();
+
+  return (
+    <Flex itemAlign="center" gap={2} className="oui-text-2xs">
+      {/* BBO Button */}
+      <Flex
+        px={3}
+        height={20}
+        justify="center"
+        itemAlign="center"
+        r="base"
+        className={cn(
+          "oui-cursor-pointer oui-select-none oui-border",
+          bbo.bboStatus === BBOStatus.ON
+            ? "oui-border-primary"
+            : "oui-border-line-12",
+          bbo.bboStatus === BBOStatus.DISABLED && "oui-cursor-not-allowed",
+        )}
+        onClick={() => {
+          if (bbo.bboStatus === BBOStatus.DISABLED) {
+            modal.dialog({
+              title: t("common.tips"),
+              size: "xs",
+              content: (
+                <Text intensity={54}>{t("orderEntry.bbo.disabled.tips")}</Text>
+              ),
+            });
+          } else {
+            bbo.toggleBBO();
+          }
+        }}
+      >
+        <Text
+          className={cn(
+            bbo.bboStatus === BBOStatus.ON && "oui-text-primary",
+            bbo.bboStatus === BBOStatus.OFF && "oui-text-base-contrast-54",
+            bbo.bboStatus === BBOStatus.DISABLED && "oui-text-base-contrast-20",
+          )}
+        >
+          {t("orderEntry.bbo")}
+        </Text>
+      </Flex>
+
+      {/* Divider */}
+      <div className="oui-h-3 oui-w-px oui-bg-line-12" />
+
+      {/* Mid Button */}
+      <Text
+        className="oui-select-none oui-cursor-pointer oui-text-primary"
+        onClick={fillMiddleValue}
+      >
+        Mid
+      </Text>
+    </Flex>
+  );
+};
